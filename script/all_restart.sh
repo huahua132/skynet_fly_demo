da34c06ddbb5ea77bf97fa72851a4b9169114cec
huahua132 <958677003@qq.com>
2024-01-24 10:12:16 +0800

    all_restart

--- script/all_restart.sh
+++ script/all_restart.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-$skynet_fly_path = "../../skynet_fly/"
+skynet_fly_path="../../skynet_fly/"
+bin_shell_path="${skynet_fly_path}/script/shell"
+make_cmd="${bin_shell_path}/make_server.sh ${skynet_fly_path}"
 
 cd world/logserver
-bash ${skynet_fly_path}binshell/make_server.sh ${skynet_fly_path}
+bash ${make_cmd}
 bash script/restart.sh load_mods.lua
 cd ../../
 
 cd games/chinese_chess
-bash ${skynet_fly_path}binshell/make_server.sh ${skynet_fly_path}
+bash ${make_cmd}
 bash script/restart.sh load_mods.lua
 cd ../../
 
 cd games/stone_scissors_cloth
-bash ${skynet_fly_path}binshell/make_server.sh ${skynet_fly_path}
+bash ${make_cmd}
 bash script/restart.sh load_mods.lua
 cd ../../
 
 cd admin/admin_server
-bash ${skynet_fly_path}binshell/make_server.sh ${skynet_fly_path}
+bash ${make_cmd}
 bash script/restart.sh load_mods.lua
 cd ../../
