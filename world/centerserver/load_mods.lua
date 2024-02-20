60953acd662d03a929edaf18ee45961e8eec4992
huahua132 <958677003@qq.com>
2024-02-20 17:33:41 +0800

    hooklog

--- world/centerserver/load_mods.lua
+++ world/centerserver/load_mods.lua
@@ -83,6 +83,7 @@ return {
 		default_arg = {
 			node_map = {
 				['hallserver'] = true,      --大厅服
+                ['logserver'] = true,       --日志服
 			},
 			watch = 'redis',  --监听redis的方式做服务发现
 		}
--- world/logserver/main.lua
+++ world/logserver/main.lua
@@ -1,7 +1,5 @@
 local skynet = require "skynet"
 local contriner_launcher = require "contriner_launcher"
-local timer_point = require "timer_point"
-local time_util = require "time_util"
 local log = require "log"
 
 skynet.start(function()
@@ -10,9 +8,5 @@ skynet.start(function()
 	contriner_launcher.run()
 	--启动集群连接入口
 	skynet.uniqueservice("cluster_server")
-
-	timer_point:new(timer_point.EVERY_MINUTE)
-    :builder(function()
-        log.info("每分钟:", os.date("[%Y%m%d %H:%M:%S",time_util.time()))
-    end)
+	skynet.exit()
 end)
