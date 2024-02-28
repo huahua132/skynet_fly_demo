36f6bf8a9fd1987a19f58e78a6fc4e7cd252c3ae
huahua132 <958677003@qq.com>
2024-02-28 15:21:10 +0800

    db install

--- dbinstall/mysql/setup.sh
+++ dbinstall/mysql/setup.sh
@@ -3,7 +3,7 @@
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
 rm -rf mysql-community-release-el7-5.noarch.rpm
-yum update
+yum update -y
 yum install -y mysql-server
 chown -R mysql:mysql /var/lib/mysql/
 mysqld --initialize
