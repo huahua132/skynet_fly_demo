895b542fb3141853b0900a4a9e4f2af8a27312b7
linchufeng <linchufeng@25qp.cn>
2024-03-11 18:03:41 +0800

    能快速运行skynet-fly 脚本

---
+++ script/make_run_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# cd 到 skynet_fly_demo 目录下后
+now_path=$(pwd)
+skynet_fly_path="${now_path}/skynet_fly/"
+echo skynet_fly_path = $skynet_fly_path
+
+bin_shell_path="${skynet_fly_path}script/shell"
+echo bin_shell_path = $bin_shell_path
+
+make_cmd="${bin_shell_path}/make_server.sh ${skynet_fly_path}"
+echo make_cmd = $make_cmd
+
+#此处可以 examples 的其他例子
+cd skynet_fly/examples/webapp
+bash ${make_cmd}
+bash script/restart.sh load_mods.lua
+cd ../../
