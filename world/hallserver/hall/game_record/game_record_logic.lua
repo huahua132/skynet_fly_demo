6717fc926aa721cb8b58c372af04bfdf9e5f571b
huahua132 <958677003@qq.com>
2024-04-13 15:25:08 +0800

    game_record

--- world/hallserver/hall/game_record/game_record_logic.lua
+++ world/hallserver/hall/game_record/game_record_logic.lua
@@ -3,7 +3,7 @@ local EVENT_ID = require "enum.EVENT_ID"
 local log = require "skynet-fly.log"
 local orm_table_client = require "skynet-fly.client.orm_table_client"
 
-local g_game_record_cli = orm_table_client:new("game_record_entity")
+local g_game_record_cli = orm_table_client:new("game_record")
 
 local M = {}
 
