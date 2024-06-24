28cf1a12ca2bf2e95b7b7cb8e80926cc6ad99533
huahua132 <958677003@qq.com>
2024-06-24 23:31:13 +0800

    prod robotnum

--- robots/chinese_chess_robot/load_mods_prod.lua
+++ robots/chinese_chess_robot/load_mods_prod.lua
@@ -12,4 +12,6 @@ cfg.share_config_m.default_arg.redis.global = redis_cfg.global
 cfg.share_config_m.default_arg.frpc_server = frpc_server_cfg.robots.chinese_chess_robot
 cfg.share_config_m.default_arg.server_cfg = server_cfg.robots.chinese_chess_robot
 
+cfg.robot_launch_m.default_arg.robot_num = 101
+
 return cfg
