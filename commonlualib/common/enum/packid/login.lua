eda8a91cc8ea50b2dab1615b3a1db61078f0fbd2
huahua132 <958677003@qq.com>
2024-08-11 19:06:30 +0800

    包名打包方式，改为更省的消息码打包方式

---
+++ commonlualib/common/enum/packid/login.lua
@@ -0,0 +1,7 @@
+--这里是主码对应子消息码 key为pb对应消息包名称
+local enum = {
+    LoginReq = 1,              --登录请求
+    LoginRes = 2,              --登录回复
+}
+
+return enum
---
+++ commonlualib/gamecommon/enum/packid/game_hall.lua
@@ -0,0 +1,7 @@
+local enum = {
+    JoinReq = 1,     --加入房间请求
+    JoinRes = 2,     --回复加入房间请求
+    HeartReq = 3,    --心跳包
+}
+
+return enum
