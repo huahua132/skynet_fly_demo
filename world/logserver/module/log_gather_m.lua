8e7dbe1487139c8c93ce51741ca3cb948ac9ca4b
huahua132 <958677003@qq.com>
2025-04-08 15:42:21 +0800

    健壮日志采集处理

--- world/logserver/module/log_gather_m.lua
+++ world/logserver/module/log_gather_m.lua
@@ -24,6 +24,10 @@ local math = math
 local string = string
 local table = table
 local setmetatable = setmetatable
+local pcall = pcall
+local next = next
+local coroutine = coroutine
+local x_pcall = x_pcall
 
 local g_IGNORE_MAP = {
     ['_log_name'] = true
@@ -31,16 +35,68 @@ local g_IGNORE_MAP = {
 local g_gater_ormobj = nil
 local g_orm_map = {}
 local g_log_info_map = {}
-local g_timer = nil
 local g_timer_point = nil
 local g_loop_timer_map = {}
+local g_try_data_map = {}
+local g_logicing_map = {}
+local g_MAX_TRY_JOIN_DATE_LIST = 10000                 --最多缓存一万条重试待插入日志，如果超过，将暂定日志采集
+local g_try_cur_num = 0
 local g_is_over_map = setmetatable({}, {__mode = 'kv'})
+local g_config = nil
 
 local g_orm_type_strs = {}
 do
     for str, type in pairs(ormtable.FIELD_TYPE) do
         g_orm_type_strs[type] = str
     end
+end
+
+local function is_need_wait()
+    if g_try_cur_num >= g_MAX_TRY_JOIN_DATE_LIST then
+        wait:wait("waiting_try")
+        skynet.sleep(math.random(1, 100))
+    end
+end
+
+local function add_try_data(tab_name, data)
+    g_try_cur_num = g_try_cur_num + 1
+    g_try_data_map[data] = {
+        try_cnt = 0,                    --重试次数
+        tab_name = tab_name,
+    }
+end
+
+local function try_insert(try_num)
+    local list = {}
+    for data, info in pairs(g_try_data_map) do
+        table.insert(list, {data = data, info = info})
+    end
+
+    table.sort(list, function(a, b) return a.info.try_cnt < b.info.try_cnt end)
+
+    for i = 1, try_num do
+        local one = list[i]
+        if one then
+            local data = one.data
+            local info = one.info
+            local orm = g_orm_map[info.tab_name]
+            local isok, ret = pcall(orm.create_one_entry, orm, data)
+            if isok and ret then
+                g_try_data_map[data] = nil
+                g_try_cur_num = g_try_cur_num - 1
+            else
+                info.try_cnt = info.try_cnt + 1
+            end
+        end
+    end
+
+    if g_try_cur_num < g_MAX_TRY_JOIN_DATE_LIST then
+        wait:wakeup("waiting_try")
+    end
+end
+
+local function try_insert_data(try_num)
+    queue(try_insert, try_num)
 end
 
 local function create_orm_obj(use_log_info, mysqlcli)
@@ -90,6 +146,8 @@ local function create_gather_loop(cluster_name, use_log_info)
     local frpc_cli = frpc_client:instance(svr_name, '.use_log'):set_svr_id(svr_id)
 
     local function gather_logic()
+        is_need_wait()
+
         local cur_time = time_util.time()
         local cur_date = tonumber(os.date("%Y%m%d", cur_time))
         local entry = g_gater_ormobj:get_one_entry(cluster_name, file_name, cur_date)
@@ -124,9 +182,7 @@ local function create_gather_loop(cluster_name, use_log_info)
                             g_is_over_map[entry] = true
                         end
                     elseif offset == last_offset then   --已读完
-                        if offset > file_size then      --文件被修改了，重新来
-                            entry:set('offset', 0)
-                        elseif date == cur_date then      --当天不管
+                        if date == cur_date then      --当天不管
                         elseif next_date == cur_date then --昨天        --继续读1个小时(读到凌晨1点)，避免卡到文件关闭的间隙导致尾部部分没有读取
                             local over_time = time_util.day_time(0, 1, 0, 0, cur_time)
                             if cur_time > over_time then
@@ -178,10 +234,11 @@ local function create_gather_loop(cluster_name, use_log_info)
                                 log.warn("wait create orm ", cluster_name, tab_name)
                                 wait:wait(tab_name)
                             else
-                                local res = orm:create_entry(data_list)
+                                local res = orm:create_entry(data_list)                         --失败一般就是网络问题或者数据库出问题了
                                 for i = 1, #res do
                                     if not res[i] then
                                         log.error("create_entry err ", tab_name, data_list[i])
+                                        add_try_data(tab_name, data_list[i])                    --记录一下，间隔时间重试
                                     end
                                 end
                             end
@@ -196,7 +253,20 @@ local function create_gather_loop(cluster_name, use_log_info)
         end
     end
 
-    local new_timer = timer:new(timer.second * 10, 0, gather_logic):after_next()
+    local function xp_gather_logic()
+        local co = coroutine.running()
+        g_logicing_map[co] = true
+        local isok, err = x_pcall(gather_logic)
+        if not isok then
+            log.error("xp_gather_logic err ", err)
+        end
+        g_logicing_map[co] = nil
+        if not next(g_logicing_map) then
+            wait:wakeup("logic_over")
+        end
+    end
+
+    local new_timer = timer:new(timer.second * (10 + math.random(1, 5)), 0, xp_gather_logic):after_next()
 
     g_loop_timer_map[time_key] = new_timer
 
@@ -240,7 +310,7 @@ function CMD.start(config)
 
         wait:wakeup("gater")
     end)
-
+    g_config = config
     local node_map = config.node_map
     for svr_name in pairs(node_map) do
         watch_syn_client.pwatch(svr_name, SYN_CHANNEL_NAME.log_desc_info .. '*', "handle_name_1", function(cluster_name, use_log_info)
@@ -252,15 +322,17 @@ function CMD.start(config)
         end)
     end
 
+    timer:new(timer.minute, 0, try_insert_data, 20)   --重试插入数据
     
     return true
 end
 
 function CMD.fix_exit()
-    if g_timer then
-        g_timer:cancel()
+    local node_map = g_config.node_map
+    for svr_name in pairs(node_map) do
+        watch_syn_client.unpwatch(svr_name, SYN_CHANNEL_NAME.log_desc_info .. '*', "handle_name_1")
     end
-
+    
     for _,timer in pairs(g_loop_timer_map) do
         timer:cancel()
     end
@@ -274,16 +346,44 @@ function CMD.fix_exit()
     end
 end
 
+function CMD.check_exit()
+    log.warn("check_exit g_try_cur_num = ", g_try_cur_num)
+    return g_try_cur_num == 0
+end
+
 function CMD.exit()
     return true
 end
 
 skynet_util.reg_shutdown_func(function()
+    local node_map = g_config.node_map
+    for svr_name in pairs(node_map) do
+        watch_syn_client.unpwatch(svr_name, SYN_CHANNEL_NAME.log_desc_info .. '*', "handle_name_1")
+    end
+
+    for _,timer in pairs(g_loop_timer_map) do
+        timer:cancel()
+    end
+
+    log.info("log_gather_m >>> try_insert_data begin")
+    try_insert_data(g_try_cur_num)
+    log.info("log_gather_m >>> try_insert_data end")
+
+    if next(g_logicing_map) then
+        log.info("log_gather_m >>> wait logic_over begin")
+        wait:wait("logic_over")
+        log.info("log_gather_m >>> wait logic_over end")
+    end
+
     log.info("log_gather_m >>> save begin")
     if g_gater_ormobj then
         g_gater_ormobj:save_change_now()
     end
     log.info("log_gather_m >>> save end")
+
+    if next(g_try_data_map) then
+        log.error("remain try_insert_data ", g_try_data_map)
+    end
 end)
 
 return CMD
