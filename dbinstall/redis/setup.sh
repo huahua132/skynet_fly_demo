9603022853460b8867e391b05d11ee7a6454b9ce
huahua132 <958677003@qq.com>
2025-08-18 22:24:00 +0800

    redis编译依赖补充

--- dbinstall/redis/setup.sh
+++ dbinstall/redis/setup.sh
@@ -4,5 +4,6 @@ wget http://download.redis.io/releases/redis-6.0.8.tar.gz
 tar -xzvf redis-6.0.8.tar.gz
 rm -rf redis-6.0.8.tar.gz
 cd redis-6.0.8
+sudo apt install -y build-essential tcl libjemalloc-devs
 make
 
