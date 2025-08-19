7b44b9e924c3386dbd2f69bb5eefc30c50030812
huahua132 <958677003@qq.com>
2025-08-19 23:41:27 +0800

    client

--- client/chinesechess_oops/build/web-mobile/cocos-js/meshopt_decoder.wasm-43855155.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/meshopt_decoder.wasm-12c0a404.wasm")}}}));
--- client/chinesechess_oops/build/web-mobile/cocos-js/spine-d11505f0.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-17c81aa0.wasm")}}}));
